5b2090935b245354b044b0892f6ab4a51bac4577
JoaquinGarcia <joaq.garcia90@gmail.com>
2023-03-22 20:20:28 -0300

    carga arch.java

---
+++ Ejercicio3Dia1.java
@@ -0,0 +1,27 @@
+        /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio3dia1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joaquín García
+ */
+public class Ejercicio3Dia1 {
+
+    public static void main(String[] args) {
+        
+        /*Scanner palabra=new Scanner (System.in);
+        System.out.println("Ingrese una palabra");
+        String ingreso=palabra.nextLine();*/
+        String ingreso="ingrese una palabnra";
+        ingreso=ingreso.toUpperCase();
+        System.out.println(ingreso);
+        ingreso=ingreso.toLowerCase();        
+        System.out.println(ingreso);
+        
+    }
+}
